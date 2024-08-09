cdb06dedc1f3835931ed840d8f68786a350978c7
Lee Inho <inhoking01@gmail.com>
2024-08-09 10:17:00 +0900

    feat. 애플 로그인 추가

--- SnapPop/ViewModels/Auth/AuthViewModel.swift
+++ SnapPop/ViewModels/Auth/AuthViewModel.swift
@@ -9,15 +9,26 @@ import Foundation
 import Firebase
 import FirebaseAuth
 import GoogleSignIn
+import AuthenticationServices
+import CryptoKit
 
 final class AuthViewModel {
-    func googleSignIn(presentingViewController: UIViewController) {
+    static let shared = AuthViewModel()
+    
+    var currentNonce: String?
+    
+    var currentUser: User? {
+        Auth.auth().currentUser
+    }
+    
+    func signInWithGoogle(on viewController: UIViewController,
+                          completion: @escaping (Result<User, Error>) -> Void) {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
         
         let config = GIDConfiguration(clientID: clientID)
         GIDSignIn.sharedInstance.configuration = config
         
-        GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController) { result, error in
+        GIDSignIn.sharedInstance.signIn(withPresenting: viewController) { result, error in
             guard error == nil else { return }
             
             guard let user = result?.user,
@@ -29,17 +40,98 @@ final class AuthViewModel {
             let credential = GoogleAuthProvider.credential(withIDToken: idToken,
                                                            accessToken: user.accessToken.tokenString)
             
-            Auth.auth().signIn(with: credential) { _, _ in
-                
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if let error = error {
+                    completion(.failure(error))
+                } else if let user = authResult?.user {
+                    completion(.success(user))
+                }
             }
         }
     }
-
-    func googleSignOut() {
+    
+    func startSignInWithAppleFlow(on viewController: UIViewController,
+                                  completion: @escaping (Result<User, Error>) -> Void) {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = viewController as? ASAuthorizationControllerDelegate
+        authorizationController.presentationContextProvider = viewController as? ASAuthorizationControllerPresentationContextProviding
+        authorizationController.performRequests()
+    }
+    
+    func signInWithApple(idToken: String, rawNonce: String, fullName: PersonNameComponents?,
+                         completion: @escaping (Result<User, Error>) -> Void) {
+        guard let _ = currentNonce else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid state: A login callback was received, but no login request was sent."])))
+            return
+        }
+        
+        let credential = OAuthProvider.appleCredential(withIDToken: idToken, rawNonce: rawNonce, fullName: fullName)
+        
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = authResult?.user {
+                completion(.success(user))
+            }
+        }
+    }
+    
+    func signOut(completion: @escaping (Result<Void, Error>) -> Void) {
         do {
             try Auth.auth().signOut()
-        } catch let signOutError as NSError {
-            print("SignOut Failed: \(signOutError.localizedDescription)")
+            completion(.success(()))
+        } catch let error {
+            completion(.failure(error))
         }
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
     }
 }
--- SnapPop/Views/Auth/SignInViewController.swift
+++ SnapPop/Views/Auth/SignInViewController.swift
@@ -9,29 +9,44 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import GoogleSignIn
+import AuthenticationServices
 
 class SignInViewController: UIViewController {
-    private let viewModel: AuthViewModel
     
     private lazy var googleSignInButton: GIDSignInButton = {
         let signInButton = GIDSignInButton()
         
         signInButton.style = .wide
         signInButton.addAction(UIAction { [weak self] _ in
-            self?.viewModel.googleSignIn(presentingViewController: self ?? UIViewController())
+            AuthViewModel.shared.signInWithGoogle(on: self ?? UIViewController()) { result in
+                switch result {
+                case .success(let user):
+                    print("Successfully signed in as user: \(user.uid)")
+                case .failure(let error):
+                    print("Error signing in: \(error.localizedDescription)")
+                }
+            }
         }, for: .touchUpInside)
         
         return signInButton
     }()
     
-    init(viewModel: AuthViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    private lazy var appleSignInButton: ASAuthorizationAppleIDButton = {
+        let signInButton = ASAuthorizationAppleIDButton()
+        
+        signInButton.addAction(UIAction { [weak self] _ in
+            AuthViewModel.shared.startSignInWithAppleFlow(on: self ?? UIViewController()) { result in
+                switch result {
+                case .success(let user):
+                    print("Successfully signed in as user: \(user.uid)")
+                case .failure(let error):
+                    print("Error signing in: \(error.localizedDescription)")
+                }
+            }
+        }, for: .touchUpInside)
+        
+        return signInButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,13 +57,47 @@ class SignInViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         view.addSubview(googleSignInButton)
+        view.addSubview(appleSignInButton)
         
         googleSignInButton.translatesAutoresizingMaskIntoConstraints = false
+        appleSignInButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             googleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            googleSignInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            googleSignInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            appleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appleSignInButton.topAnchor.constraint(equalTo: googleSignInButton.bottomAnchor, constant: 16)
         ])
     }
+}
 
+extension SignInViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = AuthViewModel.shared.currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // 파이어베이스에 애플 인증 전달
+            AuthViewModel.shared.signInWithApple(idToken: idTokenString, rawNonce: nonce, fullName: appleIDCredential.fullName) { [weak self] result in
+                switch result {
+                case .success(let user):
+                    print("Successfully signed in as user: \(user.uid)")
+                case .failure(let error):
+                    print("Error signing in: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: any Error) {
+        print("Sign in with Apple errored: \(error.localizedDescription)")
+    }
 }
