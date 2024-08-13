df2a4b1e4283b218e0e5d8d379c4e6ea09744ee8
Smith <jjwon2149@gmail.com>
2024-08-13 16:00:50 +0900

    design: 커스텀 컬러 익스텐션 추가 - Color Asset 추가 - Color+Extensions.swift 추가

---
+++ SnapPop/Common/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  SnapPop
+//
+//  Created by 정종원 on 8/13/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customMainColor = UIColor(named: "customMain")
+    static let customButtonColor = UIColor(named: "customButton")
+    static let customBackgroundColor = UIColor(named: "customBackground")
+    static let customToggleColor = UIColor(named: "customToggle")
+}
