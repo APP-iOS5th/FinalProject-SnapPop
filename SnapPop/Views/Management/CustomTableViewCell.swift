bde4bb42090679f24af2ca58615cee41bbd5f07f
yehjinjang <j95033176@gmail.com>
2024-08-14 10:24:45 +0900

    chore: 테이블셀 mark

--- SnapPop/Views/Management/CustomTableViewCell.swift
+++ SnapPop/Views/Management/CustomTableViewCell.swift
@@ -22,6 +22,7 @@ class BaseTableViewCell: UITableViewCell {
         // 상속된 클래스에서 오버라이드하여 사용
     }
 }
+// -MARK: 제목
 
 class TitleCell: BaseTableViewCell {
     let textField: UITextField = {
@@ -42,6 +43,7 @@ class TitleCell: BaseTableViewCell {
         ])
     }
 }
+// -MARK: 메모
 
 class MemoCell: BaseTableViewCell {
     let textField: UITextField = {
@@ -62,7 +64,7 @@ class MemoCell: BaseTableViewCell {
         ])
     }
 }
-
+// -MARK: 색상
 class ColorCell: BaseTableViewCell {
     let colorPicker: UIColorWell = {
         let colorPicker = UIColorWell()
@@ -81,6 +83,7 @@ class ColorCell: BaseTableViewCell {
         ])
     }
 }
+// -MARK: 날짜
 
 class DateCell: BaseTableViewCell {
     let datePicker: UIDatePicker = {
@@ -100,25 +103,7 @@ class DateCell: BaseTableViewCell {
         ])
     }
 }
-
-class NotificationCell: BaseTableViewCell {
-    let switchControl: UISwitch = {
-        let switchControl = UISwitch()
-        switchControl.translatesAutoresizingMaskIntoConstraints = false
-        return switchControl
-    }()
-    
-    override func setupUI() {
-        super.setupUI()
-        contentView.addSubview(switchControl)
-        
-        NSLayoutConstraint.activate([
-            switchControl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            switchControl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
-        ])
-    }
-}
-
+// -MARK: 반복
 class RepeatCell: BaseTableViewCell {
     let repeatButton: UIButton = {
         let button = UIButton(type: .system)
@@ -149,7 +134,7 @@ class RepeatCell: BaseTableViewCell {
         repeatButton.showsMenuAsPrimaryAction = true
     }
 }
-
+// -MARK: 시간
 class TimeCell: BaseTableViewCell {
     let switchControl: UISwitch = {
         let switchControl = UISwitch()
@@ -164,6 +149,25 @@ class TimeCell: BaseTableViewCell {
         NSLayoutConstraint.activate([
             switchControl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
             switchControl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
+
+// -MARK: 알림
+class NotificationCell: BaseTableViewCell {
+    let switchControl: UISwitch = {
+        let switchControl = UISwitch()
+        switchControl.translatesAutoresizingMaskIntoConstraints = false
+        return switchControl
+    }()
+    
+    override func setupUI() {
+        super.setupUI()
+        contentView.addSubview(switchControl)
+        
+        NSLayoutConstraint.activate([
+            switchControl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            switchControl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
         ])
     }
 }
