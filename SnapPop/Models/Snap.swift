ad97196ea0df9ec4ed02b88f085bf1a8a9ce32c5
Jeongheeji <w030w58@ gmail.com>
2024-08-19 11:19:50 +0900

    Fix  : 초기화 메서드와 전달 인자값 오류로 인한 코드 수정 및 불필요 코드삭제

--- SnapPop/Models/Snap.swift
+++ SnapPop/Models/Snap.swift
@@ -12,13 +12,11 @@ import FirebaseStorage
 struct Snap: Identifiable, Hashable, Codable {
     @DocumentID var id: String?
     let imageUrls: [String]
-    let categoryId: String
     @ServerTimestamp var createdAt: Date?
     
-    init(id: String?, imageUrls: [String], categoryId: String, createdAt: Date?) {
+    init(id: String?, imageUrls: [String], createdAt: Date?) {
         self.id = id
         self.imageUrls = imageUrls
-        self.categoryId = categoryId
         self.createdAt = createdAt
     }
 }
@@ -29,10 +27,10 @@ extension Snap {
         let currentDate = Date() // 현재 날짜를 기준으로 생성
         
         return [
-            Snap(id: "1", imageUrls: ["snaptest1"], categoryId: "defaultCategoryId", createdAt: currentDate.addingTimeInterval(-86400)), // 1일 전
-            Snap(id: "2", imageUrls: ["snaptest2"], categoryId: "defaultCategoryId", createdAt: currentDate.addingTimeInterval(-172800)), // 2일 전
-            Snap(id: "3", imageUrls: ["snaptest3"], categoryId: "defaultCategoryId", createdAt: currentDate.addingTimeInterval(-259200)), // 3일 전
-            Snap(id: "4", imageUrls: ["snaptest4"], categoryId: "defaultCategoryId", createdAt: currentDate.addingTimeInterval(-345600))  // 4일 전
+            Snap(id: "1", imageUrls: ["snaptest1"], createdAt: currentDate.addingTimeInterval(-86400)), // 1일 전
+            Snap(id: "2", imageUrls: ["snaptest2"], createdAt: currentDate.addingTimeInterval(-172800)), // 2일 전
+            Snap(id: "3", imageUrls: ["snaptest3"], createdAt: currentDate.addingTimeInterval(-259200)), // 3일 전
+            Snap(id: "4", imageUrls: ["snaptest4"], createdAt: currentDate.addingTimeInterval(-345600))  // 4일 전
         ]
     }
 }
