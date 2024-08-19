c9247ea640c78c9aa03fc623eb7dc3f3d962cfee
jjwon2149 <jjwon2149@gmail.com>
2024-08-19 11:47:30 +0900

    feat: 커스텀 탭바 컨트롤러 생성

--- SnapPop/Views/Root/CustomTabBarController.swift
+++ SnapPop/Views/Root/CustomTabBarController.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class CustomTabBarController: UITabBarController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupTabbarLayout()
+        setupTabbarItem()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTabbarLayout() {
+        tabBar.tintColor = .customMain
+        tabBar.barTintColor = .systemGray
+        tabBar.backgroundColor = .white
     }
-    */
-
+    
+    private func setupTabbarItem() {
+        let firstViewController = TestCalendarViewController()
+        firstViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "calendar"),
+            selectedImage: UIImage(systemName: "calendar.fill")
+        )
+        
+        let secondViewController = HomeViewController()
+        secondViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let snapComparisonViewModel = SnapComparisonViewModel()
+        let thirdViewController = SnapComparisonViewController(viewModel: snapComparisonViewModel)
+        thirdViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "arrow.left.arrow.right.square"),
+            selectedImage: UIImage(systemName: "arrow.left.arrow.right.square.fill")
+        )
+        
+        let firstNavController = CustomNavigationController(rootViewController: firstViewController)
+        let secondNavController = CustomNavigationController(rootViewController: secondViewController)
+        let thirdNavController = CustomNavigationController(rootViewController: thirdViewController)
+        
+        viewControllers = [
+            firstNavController,
+            secondNavController,
+            thirdNavController
+        ]
+    }
 }
+
