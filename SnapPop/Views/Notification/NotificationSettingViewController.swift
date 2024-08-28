baacecf619908615bcc6209c1cd2f0e26a48dfcd
jjwon2149 <jjwon2149@gmail.com>
2024-08-29 01:12:32 +0900

    chore: 알림 설정뷰 추가

---
+++ SnapPop/Views/Notification/NotificationSettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationSettingViewController.swift
+//  SnapPop
+//
+//  Created by 정종원 on 8/29/24.
+//
+
+import UIKit
+
+class NotificationSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
