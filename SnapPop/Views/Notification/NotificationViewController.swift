649504595534144cd0a46cd8a4a9ed8afb1e762e
jjwon2149 <jjwon2149@gmail.com>
2024-08-29 01:03:57 +0900

    design: 알림뷰 컴포넌트, 레이아웃 추가

--- SnapPop/Views/Notification/NotificationViewController.swift
+++ SnapPop/Views/Notification/NotificationViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 class NotificationViewController: UIViewController {
     
     // MARK: - Properties
+    var shouldHideFirstView: Bool? {
+        didSet {
+            guard let shouldHideFirstView = self.shouldHideFirstView else { return }
+            self.recomenendTable.isHidden = shouldHideFirstView
+            self.managementTable.isHidden = !self.recomenendTable.isHidden
+        }
+    }
     
     // MARK: - UI Components
+    /// 추천 알림, 관리 알림 선택 SegmentedControl
     private let segmentedControl: UISegmentedControl = {
         let segControl = UISegmentedControl(items: ["추천 알림", "관리 알림"])
         segControl.selectedSegmentIndex = 0
-        segControl.backgroundColor = UIColor.white
-        segControl.selectedSegmentTintColor = UIColor.customToggle
+        segControl.backgroundColor = UIColor.customToggle
+        segControl.selectedSegmentTintColor = UIColor.white
+        segControl.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
         segControl.translatesAutoresizingMaskIntoConstraints = false
         return segControl
     }()
     
+    /// 추천 알림 테이블뷰
     private let recomenendTable = {
         let table = UITableView()
         table.backgroundColor = .customBackground
@@ -28,10 +38,13 @@ class NotificationViewController: UIViewController {
         return table
     }()
     
+    
     // 관리 알람은 title: 오늘 ~~ 하셨나요? subTitle: 오늘의 관리 확인하기
+    /// 관리 알림 테이블뷰
     private let managementTable = {
         let table = UITableView()
         table.backgroundColor = .customBackground
+        table.isHidden = true
         table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
@@ -41,6 +54,14 @@ class NotificationViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .customBackground
         
+        self.segmentedControl.selectedSegmentIndex = 0
+        self.didChangeValue(segment: self.segmentedControl)
+        
+        setupLayout() // 레이아웃 설정
+    }
+    
+    // MARK: - Methods
+    func setupLayout() {
         view.addSubviews([
             segmentedControl,
             recomenendTable,
@@ -50,19 +71,24 @@ class NotificationViewController: UIViewController {
         NSLayoutConstraint.activate([
             segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             segmentedControl.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
-            segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 10),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
             segmentedControl.heightAnchor.constraint(equalToConstant: 44),
             
             recomenendTable.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16),
-            recomenendTable.leadingAnchor.constraint(equalTo: segmentedControl.leftAnchor),
-            recomenendTable.trailingAnchor.constraint(equalTo: segmentedControl.rightAnchor),
-            recomenendTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            recomenendTable.leadingAnchor.constraint(equalTo: segmentedControl.leadingAnchor),
+            recomenendTable.trailingAnchor.constraint(equalTo: segmentedControl.trailingAnchor),
+            recomenendTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
             
+            managementTable.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 16),
+            managementTable.leadingAnchor.constraint(equalTo: segmentedControl.leadingAnchor),
+            managementTable.trailingAnchor.constraint(equalTo: segmentedControl.trailingAnchor),
+            managementTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
         ])
     }
     
-    // MARK: - Methods
-    
     // MARK: - Actions
-
+    
+    @objc func didChangeValue(segment: UISegmentedControl) {
+        self.shouldHideFirstView = segment.selectedSegmentIndex != 0
+    }
 }
