296e0b0f213144d8aadda4a2b6d88294f01b6b31
jjwon2149 <jjwon2149@gmail.com>
2024-08-29 11:28:10 +0900

    feat: 알림 설정 뷰 모델 추가 (진행중)

--- SnapPop/ViewModels/Notification/NotificationSettingViewModel.swift
+++ SnapPop/ViewModels/Notification/NotificationSettingViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+protocol NotificationSettingViewModelProtocol {
+    var categories: [Category] { get set }
+}
+
+class NotificationSettingViewModel: NotificationSettingViewModelProtocol{
+    // MARK: - Properties
+//    var categories: [Category] = []
+    var categories: [Category] = [
+        Category(userId: "user1", title: "탈모 관리", alertStatus: true),
+        Category(userId: "user1", title: "팔자주름 관리", alertStatus: false),
+        Category(userId: "user1", title: "바디 체크", alertStatus: false)
+    ]
+    
+    //MARK: - Initializer
+    init() {
+        // 파이어 베이스 데이터 가져와서 categories 넣어주기;
+    }
+    //MARK: - Methods
+}
