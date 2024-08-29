efaea627840259ba5362a62f1e1a9ae403cf774a
jjwon2149 <jjwon2149@gmail.com>
2024-08-29 10:37:19 +0900

    chore: 알림 설정뷰 테일블 셀 파일 추가

---
+++ SnapPop/Views/Notification/CategoryNotiSettingCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationSettingTableViewCell.swift
+//  SnapPop
+//
+//  Created by 정종원 on 8/29/24.
+//
+
+import UIKit
+
+class NotificationSettingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SnapPop/Views/Notification/NotificationSettingCell.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationSettingCell.swift
+//  SnapPop
+//
+//  Created by 정종원 on 8/29/24.
+//
+
+import UIKit
+
+class NotificationSettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
