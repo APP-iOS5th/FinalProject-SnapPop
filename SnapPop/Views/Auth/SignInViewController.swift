d953aa6574ed8c70e7d6c4816652dd8edecbafb6
yehjinjang <j95033176@gmail.com>
2024-09-02 14:02:46 +0900

    refactor: 로그인 뷰 행간 자간 조정

--- SnapPop/Views/Auth/SignInViewController.swift
+++ SnapPop/Views/Auth/SignInViewController.swift
@@ -17,30 +17,47 @@ class SignInViewController: UIViewController {
         let label = UILabel()
         label.numberOfLines = 0
         label.textAlignment = .center
-        
-        let text1 = NSAttributedString(string: "Snap \nP", attributes: [
-            .font: UIFont.balooChettanExtraBold(size: 77),
-            .foregroundColor: UIColor.white
-        ])
-        
+
+        // 글자 간격 설정
+        let kernValue: CGFloat = 5.0
+
+        // 행간 조절 NSMutableParagraphStyle
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = -20
+        paragraphStyle.lineHeightMultiple = 0.7
+
+        // 첫 번째 텍스트 Snap\nP 설정
+        let text1Attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.balooChettanExtraBold(size: 80),
+            .foregroundColor: UIColor.white,
+            .kern: kernValue,
+            .paragraphStyle: paragraphStyle
+        ]
+        let text1 = NSAttributedString(string: "Snap\n  P", attributes: text1Attributes)
+
+        // 롤리팝 이미지
         let imageAttachment = NSTextAttachment()
         imageAttachment.image = UIImage(named: "filledpop")?.withRenderingMode(.alwaysOriginal)
         imageAttachment.bounds = CGRect(x: 0, y: -15, width: 50, height: 50)
         let imageString = NSAttributedString(attachment: imageAttachment)
-        let text2 = NSAttributedString(string: "p", attributes: [
-            .font: UIFont.balooChettanExtraBold(size: 70),
-            .foregroundColor: UIColor.white
-        ])
-        
-        // 전체 문자열 조합
+
+        // 두 번째 텍스트 "p" 설정
+        let text2Attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.balooChettanExtraBold(size: 78),
+            .foregroundColor: UIColor.white,
+            .kern: kernValue,
+            .paragraphStyle: paragraphStyle
+        ]
+        let text2 = NSAttributedString(string: "p", attributes: text2Attributes)
+
         let completeText = NSMutableAttributedString()
         completeText.append(text1)
         completeText.append(imageString)
         completeText.append(text2)
-        
+
         label.attributedText = completeText
-        
-        // 그림자 설정
+
+        // appname shadow
         label.layer.shadowColor = UIColor.gray.cgColor
         label.layer.shadowOffset = CGSize(width: 2, height: 2)
         label.layer.shadowOpacity = 0.5
@@ -49,7 +66,7 @@ class SignInViewController: UIViewController {
         return label
     }()
 
-    
+
     // google signing button custom
     private lazy var googleSignInButton: UIButton = {
         let button = UIButton(type: .system)
@@ -134,7 +151,7 @@ class SignInViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             appName.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            appName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            appName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 240),
             
             googleSignInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             googleSignInButton.bottomAnchor.constraint(equalTo: appleSignInButton.topAnchor, constant: -16),
