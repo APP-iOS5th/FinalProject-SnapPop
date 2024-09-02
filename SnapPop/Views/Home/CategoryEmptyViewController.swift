eb985e8856ec3c334e5fc9b187461dfba92d1fc2
Lee Inho <inhoking01@gmail.com>
2024-09-02 13:43:58 +0900

    feat. 카테고리 없을 시 보여줄 뷰 추가

---
+++ SnapPop/Views/Home/CategoryEmptyViewController.swift
@@ -0,0 +1,107 @@
+//
+//  CategoryEmptyViewController.swift
+//  SnapPop
+//
+//  Created by Heeji Jung on 8/30/24.
+//
+
+import UIKit
+
+class CategoryEmptyViewController: UIViewController {
+    var viewModel: CustomNavigationBarViewModelProtocol // ViewModel 프로퍼티 추가
+    
+    private let categoryImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "CategoryEmptyBG") // 일러스트 이미지 이름
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        let text = """
+        카테고리를 추가하고,
+        스냅 사진으로 진행 상황을 쉽게 확인해보세요!
+        """
+        
+        let attributedString = NSMutableAttributedString(string: text)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 4 // 줄 간격 설정
+        paragraphStyle.alignment = .center // 가운데 정렬
+
+        attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: text.count))
+        
+        label.attributedText = attributedString
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 0 // 여러 줄을 표시하도록 설정
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("카테고리 추가하기", for: .normal)
+        button.backgroundColor = UIColor.customButtonColor
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    init(viewModel: CustomNavigationBarViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside) // 버튼 액션 추가
+    }
+
+    private func setupViews() {
+        view.addSubview(categoryImageView)
+        view.addSubview(messageLabel)
+        view.addSubview(actionButton)
+        
+        NSLayoutConstraint.activate([
+            categoryImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            categoryImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: view.bounds.height * 0.2), // 아래로 내리기 위해 여백 증가
+            categoryImageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+            categoryImageView.heightAnchor.constraint(equalTo: categoryImageView.widthAnchor), // 정사각형 유지
+            
+            messageLabel.topAnchor.constraint(equalTo: categoryImageView.bottomAnchor, constant: view.bounds.height * 0.1), // 아래로 내리기 위해 여백 증가
+            messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.bounds.width * 0.05), // 상대값으로 변경
+            messageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -view.bounds.width * 0.05), // 상대값으로 변경
+            
+            actionButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: view.bounds.height * 0.1), // 아래로 내리기 위해 여백 증가
+            actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            actionButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            actionButton.heightAnchor.constraint(equalToConstant: 50),
+            actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+        ])
+    }
+    
+   @objc private func handleCategoryEmptycheck() {
+        // 카테고리 상태가 변경되었을 때의 처리
+        print("CategoryEmptyViewController: Category state changed")
+        // UI 업데이트 로직 추가 (예: 뷰를 닫거나 다른 작업 수행)
+        //dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func actionButtonTapped() {
+        let sheetViewController = CategorySettingsViewController(viewModel: self.viewModel) // 기존 ViewModel 인스턴스 사용
+        // 시트 프레젠테이션 설정
+        if let sheet = sheetViewController.sheetPresentationController {
+            sheet.detents = [.medium()] // 시트 크기 설정
+            sheet.prefersGrabberVisible = true // 그랩바 표시
+        }
+        self.present(sheetViewController, animated: true, completion: nil) // 뷰 컨트롤러 표시
+    }
+}
--- SnapPop/Views/Root/CustomTabBarController.swift
+++ SnapPop/Views/Root/CustomTabBarController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Combine
 
 class CustomTabBarController: UITabBarController {
+    
+    private var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
@@ -15,6 +19,8 @@ class CustomTabBarController: UITabBarController {
         setupTabbarLayout()
         setupTabbarItem()
         selectedIndex = 1
+        
+        bind()
     }
     
     private func setupTabbarLayout() {
@@ -39,12 +45,23 @@ class CustomTabBarController: UITabBarController {
         )
         
         let customNavViewModel = CustomNavigationBarViewModel()
-        let secondViewController = HomeViewController(navigationBarViewModel: customNavViewModel)
-        secondViewController.tabBarItem = UITabBarItem(
-            title: "",
-            image: UIImage(systemName: "house"),
-            selectedImage: UIImage(systemName: "house.fill")
-        )
+        var secondViewController: UIViewController
+        
+        if let currentCategoryId = UserDefaults.standard.string(forKey: "currentCategoryId") {
+            secondViewController = HomeViewController(navigationBarViewModel: customNavViewModel)
+            secondViewController.tabBarItem = UITabBarItem(
+                title: "",
+                image: UIImage(systemName: "house"),
+                selectedImage: UIImage(systemName: "house.fill")
+            )
+        } else {
+            secondViewController = CategoryEmptyViewController(viewModel: customNavViewModel)
+            secondViewController.tabBarItem = UITabBarItem(
+                title: "",
+                image: UIImage(systemName: "house"),
+                selectedImage: UIImage(systemName: "house.fill")
+            )
+        }
         
         let snapComparisonViewModel = SnapComparisonViewModel()
         let thirdViewController = SnapComparisonViewController(viewModel: snapComparisonViewModel)
@@ -64,6 +81,42 @@ class CustomTabBarController: UITabBarController {
             secondNavController,
             thirdNavController
         ]
+    }
+    
+    private func setupSecondViewController() {
+        let customNavViewModel = CustomNavigationBarViewModel()
+        var secondViewController: UIViewController
+        
+        if let currentCategoryId = UserDefaults.standard.string(forKey: "currentCategoryId") {
+            secondViewController = HomeViewController(navigationBarViewModel: customNavViewModel)
+        } else {
+            secondViewController = CategoryEmptyViewController(viewModel: customNavViewModel)
+        }
+        
+        secondViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let secondNavController = CustomNavigationBarController(viewModel: customNavViewModel, rootViewController: secondViewController)
+        
+        if var currentViewControllers = viewControllers {
+            currentViewControllers[1] = secondNavController
+            viewControllers = currentViewControllers
+        } else {
+            viewControllers = [secondNavController]
+        }
+    }
+    
+    private func bind() {
+        NotificationCenter.default.publisher(for: UserDefaults.didChangeNotification, object: nil)
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                self.setupSecondViewController()
+                self.selectedIndex = 1
+            }
+            .store(in: &cancellables)
     }
 }
 
