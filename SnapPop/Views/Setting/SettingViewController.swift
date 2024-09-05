a7e088b4bc512d4871ff71307cce065d705cf1f8
Lee Inho <inhoking01@gmail.com>
2024-09-05 21:12:57 +0900

    chore. 팝업 회원탈퇴 스타일 변경

--- SnapPop/Views/Setting/SettingViewController.swift
+++ SnapPop/Views/Setting/SettingViewController.swift
@@ -239,7 +239,7 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     private func showDeleteAccountAlert() {
         let alert = UIAlertController(title: "SnapPop", message: "정말 탈퇴 하시겠습니까? 모든 데이터가 삭제됩니다", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "회원탈퇴", style: .default, handler: { [weak self] _ in
+        alert.addAction(UIAlertAction(title: "회원탈퇴", style: .destructive, handler: { [weak self] _ in
             AuthViewModel.shared.deleteCurrentUser(on: self ?? UIViewController())
         }))
         alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
